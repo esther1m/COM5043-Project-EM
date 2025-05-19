11de8499578fd3c1b75146ebe10f68df3ac1fcb3
esther1m <146996363+esther1m@users.noreply.github.com>
2025-05-19 21:11:47 +0100

    initalising the productclass and constructormethod

---
+++ src/Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    //class Product, initalising the variables
+    private int productId;
+    private String productName;
+    private float productPrice;
+
+    //the constructor method for class Product
+    private Product (int productId, String productName, float productPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+}
