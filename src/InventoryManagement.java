7f78b7a73e50140e1f7316d87eb81c614be8ea6a
esther1m <146996363+esther1m@users.noreply.github.com>
2025-05-22 15:42:24 +0100

    adding initial methods to inventory management

--- src/InventoryManagement.java
+++ src/InventoryManagement.java
@@ -1,2 +1,24 @@
+//class that handles the inventory management
+import java.util.HashMap;
 
+public class InventoryManagement{
+    private HashMap <Integer, Product> productsInventory;
+
+    private InventoryManagement(){
+        productsInventory = new HashMap<>();
+    }
+
+    //adding products to the hashmap
+    private void addProducts(Product product){
+        int id = product.getProductId();
+        productsInventory.put(id,product);
+    }
+
+    //removing products from the hashmap
+    private void removeProducts(Product product){
+        int id =product.getProductId();
+        productsInventory.remove(id);
+    }
+    
+}
 
