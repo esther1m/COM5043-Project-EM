c3c8e37188251256062c02eee60bc8ae0afa6b20
esther1m <146996363+esther1m@users.noreply.github.com>
2025-05-22 15:32:00 +0100

    added inital methods to class product

--- src/Product.java
+++ src/Product.java
@@ -3,11 +3,47 @@ public class Product {
     private int productId;
     private String productName;
     private float productPrice;
+    private int productQuantity;
 
     //the constructor method for class Product
-    private Product (int productId, String productName, float productPrice) {
+    private Product (int productId, String productName, float productPrice, int productQuantity) {
         this.productId = productId;
         this.productName = productName;
         this.productPrice = productPrice;
+        this.productQuantity = productQuantity;
+    }
+
+    //getter methods, returns the variables of Product instance
+    public int getProductId(){
+        return productId;
+    }
+
+    private String getProductName(){
+        return productName;
+    }
+
+    private float getProductPrice(){
+        return productPrice;
+    }
+
+    private int getProductQuantity(){
+        return productQuantity;
+    }
+
+    //adding stock to product
+    private void addStock(int amount){
+        if (amount > 0){
+            productQuantity = productQuantity + amount;
+        }
+    }
+
+    //removing stock from product
+    private void removeStock(int amount){
+        if (amount > 0) {
+            productQuantity = productQuantity - amount;
+            if (productQuantity == 0){
+                //place a new order from supplier to restock
+            }
+        }
     }
 }
