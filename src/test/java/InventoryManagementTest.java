0d7b51ec93654f817a4ace5227ecc47241a4b419
esther1m <146996363+esther1m@users.noreply.github.com>
2025-05-26 21:59:19 +0100

    added test, but broke testAddProduct

--- src/test/java/InventoryManagementTest.java
+++ src/test/java/InventoryManagementTest.java
@@ -4,16 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class InventoryManagementTest {
     //testing that the addProduct function works as expected
+    InventoryManagement test_inventory = new InventoryManagement();
+    Product test_product0 = new Product(0, "test", 3.00, 50);
+    Product test_product1 = new Product(1, "Second test", 6.00, 40);
+    Product checktestproduct = test_inventory.getProductById(0);
+
     @Test
     void testAddProduct(){
-        InventoryManagement test_inventory = new InventoryManagement();
-        Product test_product = new Product(0, "test", 3.00, 50);
-
-        test_inventory.addProduct(test_product);
-        Product checktestproduct = test_inventory.getProductById(0);
+        test_inventory.addProduct(test_product0);
 
         assertNotNull(checktestproduct, "Product should be added with no errors");
-        assertEquals("test", test_product.getProductName());
+        assertEquals("test", test_product0.getProductName());
+    }
+
+    @Test
+    void testRemoveProduct(){
+        test_inventory.removeProduct(test_product0);
+        assertNull(checktestproduct, "Product should be remove with no errors");
     }
 }
 
