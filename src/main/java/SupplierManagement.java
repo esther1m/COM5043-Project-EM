2ec3172077e59e40dda200bca2cf4d4f7ea295c1
esther1m <146996363+esther1m@users.noreply.github.com>
2025-05-27 22:28:31 +0100

    added the supplier management class

---
+++ src/main/java/SupplierManagement.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class SupplierManagement {
+    ArrayList <Supplier> supplierList;
+
+    public SupplierManagement(){
+        supplierList = new ArrayList<>();
+    }
+
+    //allowing for suppliers to be added to the array
+    public void addSupplierToList(Supplier supplier){
+        supplierList.add(supplier);
+    }
+
+    //deleting suppliers from the array
+    public void deleteSupplierFromList(Supplier supplier){
+        supplierList.remove(supplier);
+    }
+
+    //updating supplier contact details
+    public void updateSupplierContactDetails(Supplier supplier, int phoneNumber, String email){
+        supplier.setSupplierPhoneNumber(phoneNumber);
+        supplier.setSupplierEmail(email);
+    }
+}
