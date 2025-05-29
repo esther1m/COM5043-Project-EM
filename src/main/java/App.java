1c0be1d3a7b9c398194cf156590f7dc50d99774a
esther1m <146996363+esther1m@users.noreply.github.com>
2025-05-29 23:36:11 +0100

    Started the menu

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,14 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        System.out.println("Welcome to the Warehouse Management System.");
+        System.out.println("Please select an option:");
+        System.out.println("1. View Order History");
+        System.out.println("2. View Products");
+        //System.out.println("3. Add Products");
+        System.out.println("3. Place an Order");
+        System.out.println("4. View Suppliers");
+        //System.out.println("5. Edit Supplier Details");
+        //System.out.println("6. View Financial Report");
+        System.out.println("7. Exit");
 }
