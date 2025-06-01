8203e247dbd3e1df446caec3ca2bdca580095154
esther1m <146996363+esther1m@users.noreply.github.com>
2025-06-01 21:34:54 +0100

    fixing tests logic so it passes hopefully

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -7,10 +7,12 @@ import java.util.*;
 
 public class OrderTest {
     Order test_order0;
+    SupplierManagement test_supplierManagement;
     
     @BeforeEach
     void setup(){
         test_order0 = new Order(0, 0);
+        test_supplierManagement = new SupplierManagement();
     }
 
     @Test
@@ -29,17 +31,21 @@ public class OrderTest {
 
     @Test
     void testAddProductsToOrder(){
-        Product test_product = new Product(0, "Test", 7.00, 80);
+        int supplierid_1 = test_supplierManagement.defaultSupplier1.getSupplierId();
+      
+        Product test_product = new Product(0, "Test", 7.00, 80, supplierid_1);
         test_order0.addProductsToOrder(test_product, 70);
 
         assertEquals(10, test_product.getProductQuantity());
         assertEquals("Order complete", test_order0.getStatus());
     }
-
+//incomplete
     @Test
     void testAddMultipleProductsToOrder(){
-        Product test_product = new Product(0, "Test", 7.00, 80);
-        Product test_product1 = new Product(1, "Test1", 7.00, 30);
-        Product test_product2 = new Product(2, "Test2", 6.00, 10);
+        int supplierid_1 = test_supplierManagement.defaultSupplier1.getSupplierId();
+        int supplierid_2 = test_supplierManagement.defaultSupplier2.getSupplierId();
+        Product test_product = new Product(0, "Test", 7.00, 80, supplierid_1);
+        Product test_product1 = new Product(1, "Test1", 7.00, 30, supplierid_1);
+        Product test_product2 = new Product(2, "Test2", 6.00, 10, supplierid_2);
     }
 }
