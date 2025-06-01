88206b14d19323b632534d426a2a26a7e944d223
esther1m <146996363+esther1m@users.noreply.github.com>
2025-06-01 23:28:26 +0100

    please.

--- src/test/java/SupplierTest.java
+++ src/test/java/SupplierTest.java
@@ -17,6 +17,8 @@ public class SupplierTest {
     void setup(){
         test_supplier = new Supplier( "Test", 09876543210f, "test@email.com");
         int test_supplier_id = test_supplier.getSupplierId();
+        supplierManagement = new SupplierManagement();
+        supplierManagement.addSupplierToList(test_supplier);
         test_product4 = new Product(0, "Test", 5.70, 150, test_supplier_id, supplierManagement);
         
     }
@@ -30,7 +32,7 @@ public class SupplierTest {
         String test_supplier_checkemail = test_supplier.getSupplierEmail();
         ArrayList <Product> test_supplier_checklist = test_supplier.getSupplierOrderHistory();
 
-        assertEquals(21, test_supplier_checkid);
+        assertEquals(0, test_supplier_checkid);
         assertEquals("Test", test_supplier_checkname);
         assertEquals(09876543210f, test_supplier_checkphone);
         assertTrue(test_supplier_checklist.isEmpty());
