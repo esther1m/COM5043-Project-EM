cf0ecb485401b15e381cdb3baddfe44a7769b63b
esther1m <146996363+esther1m@users.noreply.github.com>
2026-02-23 23:45:38 +0000

    cleaned order test class

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,74 +1,93 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.*;
 
 public class OrderTest {
-    Order test_order0;
-    SupplierManagement test_supplierManagement;
+    private Order test_order0;
+    private SupplierService test_supplierManagement;
+    private Supplier test_supplier1;
+    private HashMap <Product, Integer> products;
+
+    private Product test_product0;
+    private Product test_product1;
     
     @BeforeEach
     void setup(){
-        test_order0 = new Order(0, 0);
-        test_supplierManagement = new SupplierManagement();
-        Supplier test_supplier1 = new Supplier("test", 07875432578f, "email@email.com");
+        
+        test_supplierManagement = new SupplierService();
+        test_supplier1 = new Supplier("test", "07875432578", "email@email.com");
         test_supplierManagement.addSupplierToList(test_supplier1);
+
+        int supplierid = test_supplier1.getSupplierId();
+
+        test_product0 = new Product("test", 3.00, 50, supplierid, 50, 15);
+        test_product1 = new Product("Second test", 6.00, 40, supplierid, 60, 20);
+
+        test_order0 = new Order(6786);
     }
 
     @Test
     void testCreateOrder(){
 
-
-        int test_id = test_order0.getId();
-        //int test_customerId = test_order0.get
-        double test_totalCost = test_order0.getTotalCost();
-        String test_status = test_order0.getStatus();
-
-        assertEquals(0, test_id);
-        assertEquals(0, test_totalCost);
-        assertEquals("Processing", test_status);
+        
     }
-
+//testig that adding products to an order reduces product stock and increases the total
     @Test
     void testAddProductsToOrder(){
-        Supplier test_supplier = test_supplierManagement.defaultSupplier1;
-        int supplierid_1 = test_supplier.getSupplierId();
+        //Supplier test_supplier = test_supplierManagement.defaultSupplier1;
+        //int supplierid_1 = test_supplier.getSupplierId();
       
-        Product test_product = new Product("Test", 7.00, 80, supplierid_1, test_supplierManagement);
-        test_order0.addProductsToOrder(test_product, 70);
+       // Product test_product = new Product("Test", 7.00, 80, supplierid_1, test_supplierManagement);
+        boolean result1 = test_order0.addProductsToOrder(test_product0, 30);
+        boolean result2 = test_order0.addProductsToOrder(test_product1, 10);
 
-        assertEquals(160, test_product.getProductQuantity());
+        assertTrue(result1, "Expected to succeed");
+        assertTrue(result2, "Expected to succeed");
+
+        assertEquals(20, test_product0.getProductQuantity());
+        assertEquals(30, test_product1.getProductQuantity());
+
+        assertEquals(150, test_order0.getTotalCost());
         assertEquals("Order complete", test_order0.getStatus());
     }
+/* 
 //testing adding multiple products to one order
     @Test
     void testAddMultipleProductsToOrder(){
-        Order test_order2 = new Order(70, 553);
+        //Order test_order2 = new Order(70, 553);
         int supplierid_1 = test_supplierManagement.defaultSupplier1.getSupplierId();
         int supplierid_2 = test_supplierManagement.defaultSupplier2.getSupplierId();
-        Product test_product = new Product("Test", 7.00, 80, supplierid_1, test_supplierManagement);
-        Product test_product1 = new Product("Test1", 7.00, 30, supplierid_1, test_supplierManagement);
-        Product test_product2 = new Product("Test2", 6.00, 10, supplierid_2, test_supplierManagement);
+        
 
-        test_order2.addProductsToOrder(test_product, 4);
-        test_order2.addProductsToOrder(test_product1, 1);
-        test_order2.addProductsToOrder(test_product2, 1);
+        test_order0.addProductsToOrder(test_product0, 4);
+        test_order0.addProductsToOrder(test_product1, 1);
+        //test_order2.addProductsToOrder(test_product2, 1);
 
-        assertEquals(76, test_product.getProductQuantity());
+        assertEquals(76, test_product0.getProductQuantity());
         assertEquals(29, test_product1.getProductQuantity());
-        assertEquals(159, test_product2.getProductQuantity());
-        assertEquals("Order complete", test_order2.getStatus());
+        //assertEquals(159, test_product2.getProductQuantity());
+        assertEquals("Order complete", test_order0.getStatus());
 
-    }
+    }*/
 
+    //testing how system handles null product
     @Test
     void testOrderStatusAfterInvalidAdd() {
-        Order test_order1 = new Order(90, 679);
-        assertFalse(test_order1.addProductsToOrder(null, 0));
-        assertEquals("Failed to place order of null due to stock issues", test_order1.getStatus());
-}
+        //Order test_order1 = new Order(90);
+        assertFalse(test_order0.addProductsToOrder(null, 0));
+        assertEquals("Failed to place order of " + null, test_order0.getStatus());
 
+        boolean result1 = test_order0.addProductsToOrder(null, 30);
+        assertFalse(result1, "Adding null product should return false");
+}
+    //testing the system puts the order as processing in its initial state
+    @Test
+    void orderInitialState(){
+        assertEquals("Processing", test_order0.getStatus());
+        assertEquals(0.0, test_order0.getTotalCost());
+        assertTrue(test_order0.getId() > 0, "Order id should be a positive int");
+    }
 }
