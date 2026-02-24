e34aa25961c2445d6f0b836c648ff4c1c3e6908c
esther1m <146996363+esther1m@users.noreply.github.com>
2026-02-24 17:55:07 +0000

    fixed supplier service tests

--- src/test/java/SupplierManagementTest.java
+++ src/test/java/SupplierManagementTest.java
@@ -1,6 +1,3 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -10,36 +7,39 @@ import org.junit.jupiter.api.Test;
 
 public class SupplierManagementTest {
 
-    SupplierManagement test_suppliermanagement;
+    SupplierService test_suppliermanagement;
     Supplier test_supplier;
 
     @BeforeEach
     void setup(){
-        test_suppliermanagement = new SupplierManagement();
-        test_supplier = new Supplier("test supplier", 0363454637764f, "test@supplier.com");
+        test_suppliermanagement = new SupplierService();
+        test_supplier = new Supplier("test supplier", "0363454637764", "test@supplier.com");
     }
 
+    //checking the default suppliers exist when constructing
+    @Test
+    void testDefaultSuppliers(){
+        assertNotNull(test_suppliermanagement.defaultSupplier1);
+        assertNotNull(test_suppliermanagement.defaultSupplier2);
+    }
+    
     //checking a supplier can be successfuly be added to the method by checking the array is not null
     @Test
     void testAddSupplierToList(){
-        test_suppliermanagement.addSupplierToList(test_supplier);
-        assertNotNull(test_suppliermanagement.supplierList);
+        
+        //assertNotNull(test_suppliermanagement.supplierList);
+        boolean added = test_suppliermanagement.addSupplierToList(test_supplier);
+        assertTrue(added);
     }
 
-    //testing a supplier can be successfuly removed from the method by checking the array is null
+    //testing a supplier cannot be duplicated
     @Test
-    void testDeleteSupplierFromList(){
-        test_suppliermanagement.deleteSupplierFromList(test_supplier.getSupplierId());
-        assertEquals(2, test_suppliermanagement.supplierList.size());
-    }
+    void testAddSupplierToList_duplicate(){
+        boolean added = test_suppliermanagement.addSupplierToList(test_supplier);
+        assertTrue(added);
 
-    //testing updatng supplier contacts
-    @Test
-    void testUpdateSupplierContacts(){
-        test_suppliermanagement.updateSupplierContactDetails(test_supplier, 0, null);
-
-        assertEquals(test_supplier.getSupplierPhoneNumber(), 0);
-        assertNull(test_supplier.getSupplierEmail());
+        boolean added2 = test_suppliermanagement.addSupplierToList(test_supplier);
+        assertFalse(added2);
     }
 
     //testing adding null suppliers, should return false
@@ -47,5 +47,55 @@ public class SupplierManagementTest {
     void testAddNullSupplier() {
         assertFalse(test_suppliermanagement.addSupplierToList(null));
     }
+
+    //testing a supplier can be successfuly removed from the method by checking the array size goes back to default (2)
+    @Test
+    void testDeleteSupplierFromList(){
+        test_suppliermanagement.addSupplierToList(test_supplier);
+        test_suppliermanagement.deleteSupplierFromList(test_supplier.getSupplierId());
+        assertEquals(2, test_suppliermanagement.supplierList.size());
+    }
+
+    //testing deleting a supplier removes them from the list
+    @Test
+    void testDeleteSupplier_deletesFromList(){
+        Supplier remove = new Supplier("delete test", "09090909090", "test@delete.com");
+        test_suppliermanagement.addSupplierToList(remove);
+        int id = remove.getSupplierId();
+        //ensuring succesful addition 
+        assertEquals(remove, test_suppliermanagement.getSupplierById(id));
+
+        //delete
+        test_suppliermanagement.deleteSupplierFromList(id);
+        assertNull(test_suppliermanagement.getSupplierById(id));
+
+    }
+
+    //testing deleting a nonexistent supplier is handed gracefully
+    @Test
+    void testDeleteSupplier_nonexistent(){
+        assertDoesNotThrow(() -> test_suppliermanagement.deleteSupplierFromList(9999999));
+    }
+
+    //testing updatng supplier contacts
+    @Test
+    void testUpdateSupplierContacts(){
+        test_suppliermanagement.addSupplierToList(test_supplier);
+        test_suppliermanagement.updateSupplierContactDetails(test_supplier, "023232", "test@test");
+
+        assertEquals(test_supplier.getSupplierPhoneNumber(), "023232");
+        //assertNull(test_supplier.getSupplierEmail());
+        assertEquals(test_supplier.getSupplierEmail(), "test@test");
+    }
+
+    //testing getting supplier by invalid id returns null
+    @Test
+    void testGetSupplierbyNullId(){
+        assertNull(test_suppliermanagement.getSupplierById(999999));
+    }
+
+
+
+    
 
 }
